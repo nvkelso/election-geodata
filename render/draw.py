985bc5d8bd1af4c96e6830963cc6ff7e7edacdf7
Michal Migurski <mike-git@teczno.com>
2017-03-16 23:48:01 -0500

    Put CONUS in a corner

--- render/draw.py
+++ render/draw.py
@@ -4,8 +4,8 @@ import mapnik, sys, os
 png_filename = os.path.abspath(sys.argv[1])
 os.chdir(os.path.dirname(__file__))
 
-map = mapnik.Map(1780, 1190)
+map = mapnik.Map(1780, 1250)
 mapnik.load_map(map, 'style.xml')
-bbox=(mapnik.Envelope(-2110000, -2170000, 2570000, 770000))
+bbox=(mapnik.Envelope(-2700000, -2800000, 2570000, 770000))
 map.zoom_to_box(bbox)
 mapnik.render_to_file(map, png_filename)

2bdac12e6cacbba1098bc1fa10c03973eb0bef49
Michal Migurski <mike-git@teczno.com>
2017-03-18 12:47:32 -0500

    Switched to local projections for Hawaii and Alaska

--- render/draw.py
+++ render/draw.py
@@ -12,12 +12,16 @@ mapnik.render_to_file(map1, conus_path)
 
 map2 = mapnik.Map(580, 480)
 mapnik.load_map(map2, 'style.xml')
-bbox=(mapnik.Envelope(-4400000, 1350000, -1450000, 3850000))
+# switch to http://spatialreference.org/ref/epsg/3338/
+map2.srs = '+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs'
+bbox=(mapnik.Envelope(-1250000, 380000, 1550000, 2400000))
 map2.zoom_to_box(bbox)
 mapnik.render_to_file(map2, alaska_path)
 
-map3 = mapnik.Map(300, 340)
+map3 = mapnik.Map(360, 290)
 mapnik.load_map(map3, 'style.xml')
-bbox=(mapnik.Envelope(-5800000, -1100000, -5400000, -380000))
+# switch to http://spatialreference.org/ref/epsg/2783/
+map3.srs = '+proj=tmerc +lat_0=20.33333333333333 +lon_0=-156.6666666666667 +k=0.999966667 +x_0=500000 +y_0=0 +ellps=GRS80 +units=m +no_defs'
+bbox=(mapnik.Envelope(40000, -170000, 700000, 220000))
 map3.zoom_to_box(bbox)
 mapnik.render_to_file(map3, hawaii_path)

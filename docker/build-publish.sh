5b1b9b87372f28b9114c5165b373f7a06a6e2bb8
Michal Migurski <mike@remix.com>
2017-09-13 15:37:50 -0700

    Added more Github status updates as curl commands instead of a Python script

--- docker/build-publish.sh
+++ docker/build-publish.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -ex
+#!/bin/bash -ex
 
 # First, check to see if there are any local changes
 # in the repo and refuse to continue if any are found.
@@ -6,20 +6,43 @@ git update-index -q --refresh
 
 if git diff-index --quiet HEAD --; then
     # Remember what Git commit and branch we are on.
-    export GIT_SHA1=`git rev-parse HEAD`
-    export GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+    GIT_SHA1=`git rev-parse HEAD`
+    GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+    STATUS_URL="https://api.github.com/repos/$GITHUB_USERNAME/$GITHUB_REPONAME/commits/$GIT_SHA1/statuses"
 else
     echo 'Boo: there are local uncommitted changes.'
     exit 1
 fi
 
+# Tell Github we're working on it.
+curl -v \
+    -d '{"state":"pending","context":"elections","description":"Working on it..."}' \
+    -u "$GITHUB_TOKEN:x-oauth-basic" \
+    $STATUS_URL
+
+# Tell Github we failed.
+function admit_defeat
+{
+    curl -v \
+        -d '{"state":"failure","context":"elections","description":"Argh."}' \
+        -u "$GITHUB_TOKEN:x-oauth-basic" \
+        $STATUS_URL
+    
+    exit 1
+}
+
 # Render an image and a Geopackage output.
-make clean out/render.png out/nation.gpkg
+make clean out/render.png out/nation.gpkg || admit_defeat
 
 # Upload render for this commit and tell Github about it.
 RENDER_PATH=$S3_BUCKET/commits/$GIT_SHA1/render.png
 aws --region us-east-1 s3 cp --acl public-read out/render.png s3://$RENDER_PATH
-scripts/update-status.py https://s3.amazonaws.com/$RENDER_PATH
+
+# Tell Github everything worked.
+curl -v \
+    -d '{"state":"success","context":"elections","description":"Everything is awesome.","target_url":"https://s3.amazonaws.com/'$RENDER_PATH'"}' \
+    -u "$GITHUB_TOKEN:x-oauth-basic" \
+    $STATUS_URL
 
 # Deploy if we're on the deploy branch.
 if [ $GIT_BRANCH = 'master' ]; then
